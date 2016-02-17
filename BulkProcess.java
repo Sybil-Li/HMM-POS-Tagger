98a80fec285ac7993f5ef1e6bacb41d817b317e8
Sybil_Li <li.bing.qing.2009@vjc.sg>
2016-02-17 10:59:25 +0000

    Finished loop frame to process all files and save them into new folder. Tried to use Files and Paths API, did not work :/

--- BulkProcess.java
+++ BulkProcess.java
@@ -3,25 +3,49 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.*;
 
 public class BulkProcess {
 	public static void main (String[] args) {
-		String posString = "";
-
 		// Read file content into String
-		Path file_path = Paths.get("WSJ-2-12/02", "WSJ_0200.POS");
+		// try {
+		// 	Files.walk(Paths.get("/Users/Sybil/Documents/College/Oxford/Computational Linguistics/Practical/WSJ-2-12")).forEach(file_path -> {
+		// 		try {
+		// 			Files.walk(Paths.get(file_path.toString())).forEach(filePath -> {
+		// 			    processFile(filePath);
+		// 			});
+		// 		} catch (IOException e) {
+		// 	    	System.out.println("1 " + e);
+		// 		}
+		// 	});
+		// } catch (IOException e) {
+		// System.out.println("2 " + e);
+		// }
+		for (int i = 2; i < 10; i++){
+			for (int j = i*100; j < i*100+100; j++) {
+				Path file_path = Paths.get("WSJ-2-12/0" + i, "WSJ_0" + j + ".POS");
+				processFile(file_path);
+			}	
+		}
+		for (int i = 10; i < 13; i++){
+			for (int j = i*100; j < i*100+100; j++) {
+				Path file_path = Paths.get("WSJ-2-12/" + i, "WSJ_" + j + ".POS");
+				processFile(file_path);
+			}
+		}
+
+	}
+
+	public static void processFile(Path file_path) {
+		String posString = "";
 		try {
-	      byte[] posArray = Files.readAllBytes(file_path);
+	    	byte[] posArray = Files.readAllBytes(file_path);
 
-	      posString = new String(posArray);
-	      //System.out.println(posString);
+	    	posString = new String(posArray);
+	    	//System.out.println(posString);
 	    } catch (IOException e) {
-	      System.out.println(e);
+	    	System.out.println("3 " +e);
 	    }
-
 	    // split String to extract useful content
 	    String [] tokens = posString.split(" ");
 	    // for (int i = 0; i < tokens.length; i++) {
@@ -32,7 +56,7 @@ public class BulkProcess {
 
 	    // Write to a different file
 	    try {
-			File file = new File("WSJ_0200.txt");
+			File file = new File("processed/"+file_path.getFileName().toString());
 
 			// if file doesnt exists, then create it
 			if (!file.exists()) {
